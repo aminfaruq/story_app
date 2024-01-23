cc5c7802f60410dd88938ea650e4826e8ada2727
Amin Faruq <aminfaruk.fa@gmail.com>
2024-01-23 20:21:06 +0800

    fix error main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -56,75 +56,79 @@ class _MainAppState extends State<MainApp> {
           value: HomeProvider(),
         ),
       ],
-      child: FutureBuilder<void>(
-        future: authProvider.initialize(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // Initialize router here
-            router = GoRouter(
-              routes: [
-                // Routes configuration remains the same
-                GoRoute(
-                  path: '/splash',
-                  name: 'splash',
-                  builder: (context, state) => const SplashScreen(),
-                ),
-                GoRoute(
-                  path: '/login',
-                  name: 'login',
-                  builder: (context, state) => const LoginPage(),
-                  routes: [
-                    GoRoute(
-                      path: 'register',
-                      name: 'register',
-                      builder: (context, state) => const RegisterPage(),
-                    )
-                  ],
-                ),
-                GoRoute(
-                    path: '/',
-                    name: 'home',
-                    builder: (context, state) => const HomePage(),
+      child: MaterialApp(
+        theme: CustomTheme.appTheme,
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder<void>(
+          future: authProvider.initialize(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // Initialize router here
+              router = GoRouter(
+                routes: [
+                  // Routes configuration remains the same
+                  GoRoute(
+                    path: '/splash',
+                    name: 'splash',
+                    builder: (context, state) => const SplashScreen(),
+                  ),
+                  GoRoute(
+                    path: '/login',
+                    name: 'login',
+                    builder: (context, state) => const LoginPage(),
                     routes: [
                       GoRoute(
-                          path: 'detail/:id',
-                          name: 'detail',
-                          builder: (context, state) {
-                            String storyId =
-                                state.pathParameters['id'] ?? 'no id';
-                            return StoryDetailPage(storyId: storyId);
-                          }),
-                      GoRoute(
-                        path: 'add',
-                        name: 'add',
-                        builder: (context, state) => const AddStoryPage(),
+                        path: 'register',
+                        name: 'register',
+                        builder: (context, state) => const RegisterPage(),
                       )
-                    ]),
-              ],
-              redirect: (context, state) {
-                if (router == null) {
-                  return '/splash';
-                } else {
-                  return null;
-                }
-              },
-              initialLocation: authProvider.isLoggedIn ? '/' : '/login',
-              debugLogDiagnostics: true,
-              routerNeglect: true,
-            );
+                    ],
+                  ),
+                  GoRoute(
+                      path: '/',
+                      name: 'home',
+                      builder: (context, state) => const HomePage(),
+                      routes: [
+                        GoRoute(
+                            path: 'detail/:id',
+                            name: 'detail',
+                            builder: (context, state) {
+                              String storyId =
+                                  state.pathParameters['id'] ?? 'no id';
+                              return StoryDetailPage(storyId: storyId);
+                            }),
+                        GoRoute(
+                          path: 'add',
+                          name: 'add',
+                          builder: (context, state) => const AddStoryPage(),
+                        )
+                      ]),
+                ],
+                redirect: (context, state) {
+                  if (router == null) {
+                    return '/splash';
+                  } else {
+                    return null;
+                  }
+                },
+                initialLocation: authProvider.isLoggedIn ? '/' : '/login',
+                debugLogDiagnostics: true,
+                routerNeglect: true,
+              );
 
-            return MaterialApp.router(
-              theme: CustomTheme.appTheme,
-              routeInformationParser: router?.routeInformationParser,
-              routerDelegate: router?.routerDelegate,
-              routeInformationProvider: router?.routeInformationProvider,
-              backButtonDispatcher: RootBackButtonDispatcher(),
-              debugShowCheckedModeBanner: false,
-            );
-          } else {
-            return const SplashScreen(); // Show a loading screen while initializing
-          }
-        },
+              return MaterialApp.router(
+                theme: CustomTheme.appTheme,
+                routeInformationParser: router?.routeInformationParser,
+                routerDelegate: router?.routerDelegate,
+                routeInformationProvider: router?.routeInformationProvider,
+                backButtonDispatcher: RootBackButtonDispatcher(),
+                debugShowCheckedModeBanner: false,
+              );
+            } else {
+              return const SplashScreen(); // Show a loading screen while initializing
+            }
+          },
+        ),
       ),
     );
   }
